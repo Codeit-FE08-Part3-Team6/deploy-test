dbbeb3235965a4864980e6d10b0342e1b6e0ba3c
leehj322 <gudwns8956@naver.com>
2024-09-03 11:53:18 +0900

    dev 이래도 잘되나

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,5 +4,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>dev 배포 확인</main>;
+  return <main>dev 배포 확인 이래도 잘되나</main>;
 }
